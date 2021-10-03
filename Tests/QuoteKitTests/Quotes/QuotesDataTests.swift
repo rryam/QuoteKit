fab7c64bdaa5c74c3bedec7a13dabac7d915c1c0
Rudrank <rudrankriyam@gmail.com>
2021-10-04 04:47:05 +0530

    Add tests for quotes data

--- Tests/QuoteKitTests/Quotes/QuotesDataTests.swift
+++ Tests/QuoteKitTests/Quotes/QuotesDataTests.swift
@@ -10,7 +10,7 @@ import XCTest
 
 final class QuotesDataTests: XCTestCase {
     @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
-    func testQuoteForParticularID() async throws {
+    func testQuoteForParticularIDAsync() async throws {
         do {
             let quote = try await QuoteKit.quote(id: "2xpHvSOQMD")
             let unwrappedQuote = try XCTUnwrap(quote)
@@ -28,20 +28,75 @@ final class QuotesDataTests: XCTestCase {
         }
     }
     
-    @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
-    func testQuotesReturnsManyQuotes() async throws {
-        do {
-            let quotes = try await QuoteKit.quotes()
-            let unwrappedQuotes = try XCTUnwrap(quotes)
-
-            XCTAssertGreaterThan(unwrappedQuotes.count, 1)
-         } catch {
-             XCTFail("Expected quotes, but failed \(error).")
-         }
+    func testQuoteForParticularID() throws {
+        let expectation = XCTestExpectation(description: #function)
+        
+        var givenQuote: Quote?
+        
+        QuoteKit.quote(id: "2xpHvSOQMD") { result in
+            switch result {
+                case .success(let quote):
+                    givenQuote = quote
+                    
+                    expectation.fulfill()
+                    
+                case .failure(let error):
+                    XCTFail("Expected quote, but failed \(error).")
+            }
+        }
+        
+        wait(for: [expectation], timeout: 3.0)
+        
+        let unwrappedQuote = try XCTUnwrap(givenQuote)
+        
+        XCTAssertEqual(unwrappedQuote.tags, ["famous-quotes", "inspirational"])
+        XCTAssertEqual(unwrappedQuote.id, "2xpHvSOQMD")
+        XCTAssertEqual(unwrappedQuote.author, "Helmut Schmidt")
+        XCTAssertEqual(unwrappedQuote.content, "The biggest room in the world is room for improvement.")
+        XCTAssertEqual(unwrappedQuote.authorSlug, "helmut-schmidt")
+        XCTAssertEqual(unwrappedQuote.length, 54)
+        XCTAssertEqual(unwrappedQuote.dateAdded, "2021-06-18")
+        XCTAssertEqual(unwrappedQuote.dateModified, "2021-06-18")
     }
     
     @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
-    func testQuotesSearchForParticularQuery() async throws {
+    func testQuotesReturnsManyQuotesAsync() async throws {
+        do {
+            let quotes = try await QuoteKit.quotes()
+            let unwrappedQuotes = try XCTUnwrap(quotes)
+            
+            XCTAssertGreaterThan(unwrappedQuotes.count, 1)
+        } catch {
+            XCTFail("Expected quotes, but failed \(error).")
+        }
+    }
+    
+    func testQuotesReturnsManyQuotes() throws {
+        let expectation = XCTestExpectation(description: #function)
+        
+        var givenQuotes: Quotes?
+        
+        QuoteKit.quotes { result in
+            switch result {
+                case .success(let quotes):
+                    givenQuotes = quotes
+                    
+                    expectation.fulfill()
+                    
+                case .failure(let error):
+                    XCTFail("Expected quotes, but failed \(error).")
+            }
+        }
+        
+        wait(for: [expectation], timeout: 3.0)
+        
+        let unwrappedQuotes = try XCTUnwrap(givenQuotes)
+        
+        XCTAssertGreaterThan(unwrappedQuotes.count, 1)
+    }
+    
+    @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+    func testQuotesSearchForParticularQueryAsync() async throws {
         do {
             let quotes = try await QuoteKit.searchQuotes(for: "biggest room")
             let unwrappedQuote = try XCTUnwrap(quotes?.results.first)
@@ -57,5 +112,36 @@ final class QuotesDataTests: XCTestCase {
         } catch {
             XCTFail("Expected quote, but failed \(error).")
         }
+    }
+    
+    func testQuotesSearchForParticularQuery() throws {
+        let expectation = XCTestExpectation(description: #function)
+        
+        var givenQuotes: Quotes?
+        
+        QuoteKit.searchQuotes(for: "biggest room") { result in
+            switch result {
+                case .success(let quotes):
+                    givenQuotes = quotes
+                    
+                    expectation.fulfill()
+                    
+                case .failure(let error):
+                    XCTFail("Expected quotes, but failed \(error).")
+            }
+        }
+        
+        wait(for: [expectation], timeout: 3.0)
+        
+        let unwrappedQuote = try XCTUnwrap(givenQuotes?.results.first)
+        
+        XCTAssertEqual(unwrappedQuote.tags, ["famous-quotes", "inspirational"])
+        XCTAssertEqual(unwrappedQuote.id, "2xpHvSOQMD")
+        XCTAssertEqual(unwrappedQuote.author, "Helmut Schmidt")
+        XCTAssertEqual(unwrappedQuote.content, "The biggest room in the world is room for improvement.")
+        XCTAssertEqual(unwrappedQuote.authorSlug, "helmut-schmidt")
+        XCTAssertEqual(unwrappedQuote.length, 54)
+        XCTAssertEqual(unwrappedQuote.dateAdded, "2021-06-18")
+        XCTAssertEqual(unwrappedQuote.dateModified, "2021-06-18")
     }
 }
