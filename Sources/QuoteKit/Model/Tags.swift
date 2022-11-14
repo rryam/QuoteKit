5f764bc48ab0169d5917dfbdeb515eccd745b3ed
Rudrank Riyam <rudrankriyam@gmail.com>
2022-11-14 06:15:17 +0530

    Add initializer to tag

--- Sources/QuoteKit/Model/Tags.swift
+++ Sources/QuoteKit/Model/Tags.swift
@@ -9,20 +9,30 @@ import Foundation
 
 public typealias Tags = [Tag]
 
-public struct Tag: Decodable, Identifiable {
+public struct Tag: Identifiable {
+    init(id: String, name: String, dateAdded: String, dateModified: String, quoteCount: Int) {
+        self.id = id
+        self.name = name
+        self.dateAdded = dateAdded
+        self.dateModified = dateModified
+        self.quoteCount = quoteCount
+    }
+
     public var id: String
     public var name: String
     public var dateAdded: String
     public var dateModified: String
     public var quoteCount: Int
-    
+
+    public var capitalisedName: String {
+        name.capitalized.replacingOccurrences(of: "-", with: " ")
+    }
+}
+
+extension Tag: Decodable {
     enum CodingKeys: String, CodingKey {
         case id = "_id"
         case name, dateAdded, dateModified
         case quoteCount
-    }
-    
-    public var capitalisedName: String {
-        name.capitalized.replacingOccurrences(of: "-", with: " ")
     }
 }
